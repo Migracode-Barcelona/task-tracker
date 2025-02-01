387a8438dcc37c06d964892a08fb903388c5db79
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2025-02-01 17:12:37 +0100

    midway version

--- web/src/components/domains/task/TaskItem/TaskItem.jsx
+++ web/src/components/domains/task/TaskItem/TaskItem.jsx
@@ -1,11 +1,36 @@
 import styles from './TaskItem.module.css';
+import PropTypes from 'prop-types';
 
-/*
-Please create the <TaskItem /> component following the design from the Figma file.
-Please make sure to add styles using CSS Modules.
-Add the necessary props to the component.
-*/
-
-export function TaskItem(props) {
-  return <div className={styles.itemWrapper}></div>;
+export function TaskItem({
+  name,
+  priority,
+  releaseDate,
+  designers,
+  projectName,
+}) {
+  return (
+    <li className={styles.taskItem}>
+      <h3>{name}</h3>
+      <p className="section">
+        <strong>Priority:</strong> {priority}
+      </p>
+      <p className="section">
+        <strong>Release Date:</strong> {releaseDate}
+      </p>
+      <p className="section">
+        <strong>Designers:</strong> {designers.join(', ')}
+      </p>
+      <p className="section">
+        <strong>Project:</strong> {projectName}
+      </p>
+    </li>
+  );
 }
+
+TaskItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  priority: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  designers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  projectName: PropTypes.string.isRequired,
+};
--- web/src/components/domains/task/TaskList/TaskList.jsx
+++ web/src/components/domains/task/TaskList/TaskList.jsx
@@ -1,14 +1,34 @@
 import styles from './TaskList.module.css';
+import PropTypes from 'prop-types';
+import { TaskItem } from '../TaskItem/TaskItem'; // Import TaskItem component
 
-/*
-Please create the <TaskList /> component following the design from the Figma file.
-Please make sure to add styles using CSS Modules.
-Create a taskItems array and return a list of <TaskItem /> components.
-*/
-
-export function TaskList() {
-  // create some task items here and return one task list for each item you have
-  const taskItems = [];
-
-  return <div className={styles.listWrapper}></div>;
+export function TaskList({ taskItems }) {
+  return (
+    <div className={styles.listWrapper}>
+      <ul>
+        {taskItems.map((task, index) => (
+          <TaskItem
+            key={index}
+            name={task.name}
+            priority={task.priority}
+            releaseDate={task.releaseDate}
+            designers={task.designers}
+            projectName={task.projectName}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+TaskList.propTypes = {
+  taskItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      priority: PropTypes.string.isRequired,
+      releaseDate: PropTypes.string.isRequired,
+      designers: PropTypes.arrayOf(PropTypes.string).isRequired,
+      projectName: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
