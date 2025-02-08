415f24eb827d1dc612cb2537fa7996b0933cd1b0
FrenchFry36 <saidakbarov@protonmail.com>
2025-02-08 15:10:05 +0100

    TaskForm component is created and added to the App component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,11 @@
 import { TaskList } from './components/domains/task/TaskList/TaskList';
+import { TaskForm } from './components/domains/task/TaskForm/TaskForm';
 
 function App() {
   return (
     <>
       <TaskList />
+      <TaskForm />
     </>
   );
 }
