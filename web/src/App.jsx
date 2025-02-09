0e9e8c1e25dc6b10dc715a3728eec0d72c752ad5
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2025-02-09 13:49:07 +0100

    Assigned a usestate with an array of object of task items

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,44 +1,45 @@
+import { useState } from 'react';
 import TaskList from './components/domains/task/TaskList/TaskList';
 
 function App() {
+  const [TaskListInfo, setTaskListInfo] = useState([
+    {
+      id: 'T1',
+      ProjectTask: 'Rework UI/UX',
+      ProjectStatus: 'Low',
+      date: '12/05/2025',
+      participants: ['Said', ' Rachel'],
+      project: 'Timer App',
+    },
+    {
+      id: 'T2',
+      ProjectTask: 'Dark Mode Toggle',
+      ProjectStatus: 'High',
+      date: '09/03/2025',
+      participants: ['Umair', ' Precious'],
+      project: 'ASA DarkMode Feature',
+    },
+    {
+      id: 'T3',
+      ProjectTask: 'Accessibility Checks',
+      ProjectStatus: 'Medium',
+      date: '15/04/2025',
+      participants: ['Micheal', ' Ricardo'],
+      project: 'Timer App',
+    },
+    {
+      id: 'T4',
+      ProjectTask: 'Notification Integration',
+      ProjectStatus: 'High',
+      date: '11/03/2025',
+      participants: ['Ebetsam', ' Deborah'],
+      project: 'Timer App',
+    },
+  ]);
+
   return (
     <>
-      <TaskList
-        TaskListInfo={[
-          {
-            id: 'T1',
-            ProjectTask: 'Rework UI/UX',
-            ProjectStatus: 'Low',
-            date: '12/05/2025',
-            participants: ['Said', ' Rachel'],
-            project: 'Timer App',
-          },
-          {
-            id: 'T2',
-            ProjectTask: 'Dark Mode Toggle',
-            ProjectStatus: 'High',
-            date: '09/03/2025',
-            participants: ['Umair', ' Precious'],
-            project: 'ASA DarkMode Feature',
-          },
-          {
-            id: 'T3',
-            ProjectTask: 'Accessibility Checks',
-            ProjectStatus: 'Medium',
-            date: '15/04/2025',
-            participants: ['Micheal', ' Ricardo'],
-            project: 'Timer App',
-          },
-          {
-            id: 'T4',
-            ProjectTask: 'Notification Integration',
-            ProjectStatus: 'High',
-            date: '11/03/2025',
-            participants: ['Ebetsam', ' Deborah'],
-            project: 'Timer App',
-          },
-        ]}
-      ></TaskList>
+      <TaskList></TaskList>
     </>
   );
 }
