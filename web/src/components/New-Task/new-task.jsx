4dc43dfde2e56161d3faebc2f62bdfb71dcb3624
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2025-02-09 14:27:02 +0100

    Added a new-task file and created a new task component

---
+++ web/src/components/New-Task/new-task.jsx
@@ -0,0 +1,56 @@
+const NewTaskForm = () => {
+  return (
+    <div className="taskBox">
+      <div>
+        <label htmlFor="taskName">Task Name: </label>
+        <input
+          type="text"
+          value={textTask}
+          id="taskName"
+          className="firstInput"
+        />
+      </div>
+      <div>
+        <label htmlFor="projectName">Project Name: </label>
+        <input
+          type="text"
+          value={textProject}
+          id="projectName"
+          className="secondInput"
+        />
+      </div>
+      <div>
+        <label htmlFor="status">Task Status:</label>
+        <select id="status" name="status">
+          <option value="option1">Low</option>
+          <option value="option2">High</option>
+          <option value="option3">Medium</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="participants">Assign Participants: </label>
+        <input
+          type="text"
+          value={participants}
+          id="projectName"
+          className="secondInput"
+        />
+      </div>
+      <div>
+        <label htmlFor="date">Pick Deadline:</label>
+        <input
+          value={deadline}
+          type="date"
+          id="date"
+          name="date"
+          min="2000-01-01"
+          max="2030-12-31"
+        />
+      </div>
+
+      <button>Create Task</button>
+    </div>
+  );
+};
+
+export default NewTaskForm;
