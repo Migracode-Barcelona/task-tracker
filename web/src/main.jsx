15733f5762d124dbff164e85ed7a55b6eb4b3d5e
Ebtesam <eebtesamm23@gmail.com>
2025-02-22 15:33:58 +0100

    Add MSW

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import TaskList from './components/domains/task/TaskList/TaskList';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
+        <Route path="/tasks" element={<TaskList />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
