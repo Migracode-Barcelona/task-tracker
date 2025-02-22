6b2b6e4cc417e46a2f4d16fe7451f0d95ef7caba
Nicolas Stuhlfauth <17216829+nicostuhlfauth@users.noreply.github.com>
2025-02-22 09:44:50 +0100

    .

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';
 import TaskList from './components/domains/task/TaskList/TaskList';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
