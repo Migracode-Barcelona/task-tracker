ccfafcbce6863f9db63b278270215aa52b4cf54d
Alejandro Sanchez <alejandro.sanchez@porsche.digital>
2025-03-10 12:42:46 +0100

    Add task management data and enable CORS in server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
+import express from 'express'
 const app = express();
+import cors from 'cors';
 import {allTasks} from './data/tasks.js';
 const port = process.env.PORT || 3333;
 
 // ...existing code...
 app.use(express.json());
+app.use(cors());
 
 app.get('/tasks', (req, res) => {
-	res.json(allTasks);
+	res.json(Array.from(allTasks.values()));
 });
 
 app.post('/tasks', (req, res) => {
